bb9ffa859d99680637c9ef294256b53baad799bf
Emil <emil@Emils-iMac.local>
2021-05-24 12:07:25 +0300

    refactoring

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-
-    @Get()
-    getHello(): string {
-        return 'It is BFF';
-    }
-}
+export class AppController { }
